e379c335c741432ed878250206630113723f9bde
manapat <manapat_kaewlai@cmu.ac.th>
2025-07-02 18:02:24 +0700

    Add onSale attribute and element

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ const app = createApp({
     const productLink = ref('https://www.camt.cmu.ac.th');
     const inStock = ref(true);
     const inventory = ref(100);
+    const onSale = ref(true);
     return {
       product,
       description,
       image,
       productLink,
       inStock,
-      inventory
+      inventory,
+      onSale
     }
   }
 
