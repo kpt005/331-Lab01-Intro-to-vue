4b75a500c7294e6acc5fb53cd2226f7e6fb03826
manapat <manapat_kaewlai@cmu.ac.th>
2025-07-02 17:53:52 +0700

    commit if-else condition
    use v conditional rendering to show instock outstock. And chained logic to show almost out of stock

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ const app = createApp({
     const description = ref('A pair of warm, comfortable boots.');
     const image = ref('./assets/images/socks_green.jpg');
     const productLink = ref('https://www.camt.cmu.ac.th');
+    const inStock = ref(true);
+    const inventory = ref(100);
     return {
       product,
       description,
       image,
-      productLink
+      productLink,
+      inStock,
+      inventory
     }
   }
 
