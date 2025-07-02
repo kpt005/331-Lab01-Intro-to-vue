50bb2b4a3a5bb9c83a13a4182c01fe494dfbefaf
manapat <manapat_kaewlai@cmu.ac.th>
2025-07-02 17:15:42 +0700

    v bind image
    setup image in main.js and use v-bind in html to bind value

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const app = createApp({
   setup() {
     const product = ref('Boots');
     const description = ref('A pair of warm, comfortable boots.');
+    const image = ref('./assets/images/socks_green.jpg');
     return {
       product,
-      description
+      description,
+      image
     }
   }
 
