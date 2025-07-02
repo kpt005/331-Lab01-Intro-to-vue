95c1efc0c4bdb1f0d4d45ae36eb193ec5b40779e
manapat <manapat_kaewlai@cmu.ac.th>
2025-07-02 17:34:38 +0700

    v bind camt url
    add camt link to product name using v-bind command

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const app = createApp({
     const product = ref('Boots');
     const description = ref('A pair of warm, comfortable boots.');
     const image = ref('./assets/images/socks_green.jpg');
+    const productLink = ref('https://www.camt.cmu.ac.th');
     return {
       product,
       description,
-      image
+      image,
+      productLink
     }
   }
 
